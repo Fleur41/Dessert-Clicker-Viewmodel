d9f9341561713f782db6a7211951d24b44415299
Fleur41 <Muriithikings456@gmail.com>
2024-09-11 17:23:05 +0300

    1st Dessert

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,7 @@ android {
 
 dependencies {
 
+    val lifecycle_version = "2.8.5"
     implementation(platform("androidx.compose:compose-bom:2024.02.00"))
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.compose.material3:material3")
@@ -75,6 +76,8 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    //Lifecycle
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifecycle_version")
 
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
